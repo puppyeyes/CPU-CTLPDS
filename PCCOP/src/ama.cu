715c31363daaed12fa51eb9c9621d12750c10705
Johan007 <johan007@126.com>
2018-04-16 12:57:50 +0800

    add some basic functions of ama

---
+++ PCCOP/src/ama.cu
@@ -0,0 +1,64 @@
+#include"ama.h"
+#include <stdio.h>
+void insertTransToAMA(Trans t,AMA *ama,Pool *pool){
+	int pool_position = pool->tail++;
+	if (pool_position > pool->size){
+			printf("pool exceed \n");
+	}
+	if(t.toState!=00000){
+		ama->count++;
+	}
+	pool->item[pool_position].state= t.toState;
+	int insertPosition=t.fromState*t.stack;
+	//尾插
+	ama->list[insertPosition].tail->next=&(pool->item[pool_position]);
+	ama->list[insertPosition].tail=&(pool->item[pool_position]);
+}
+__device__ void d_insertTransToAMA(Trans t,AMA *ama,Pool *pool){
+	int pool_position = atomicAdd(&(pool->tail), 1);
+	if (pool_position > pool->size){
+		printf("pool exceed \n");
+	}
+	if(t.toState!=00000){
+		atomicAdd(&(ama->count), 1);
+	}
+	pool->item[pool_position].state= t.toState;
+	int insertPosition=t.fromState*t.stack;
+	bool next = true;
+	while (next) {
+		int v = atomicCAS(&(ama->list[insertPosition].mutex), 0, 1);
+		if (v == 0) {
+			//在此放置你的临界区
+			//尾插
+			ama->list[insertPosition].tail->next=&(pool->item[pool_position]);
+			ama->list[insertPosition].tail=&(pool->item[pool_position]);
+			//临界区结束
+			atomicExch(&(ama->list[insertPosition].mutex), 0);
+			next = false;
+		}  //此处是安全的汇聚点
+	}  //此处是安全的汇聚点2
+}
+void deleteAMA(AMA *ama,Pool *pool){
+	ama->count=0;
+	for (int i = 0; i < AMASIZE; i++) {
+		ama->list[i].head.next=NULL;
+	}
+	pool->tail=0;
+}
+bool isEqual(AMA *ama_1,AMA *ama_2){
+	if(ama_1->count==ama_2->count){
+		return true;
+	}
+	return false;
+}
+bool isTransInAMA(Trans t,AMA *ama){
+
+	AMANode *currentNode=ama->list[t.fromState*t.stack].head.next;
+	while(currentNode!=NULL){
+		if(currentNode->state==t.toState){
+			return true;
+		}
+		currentNode=currentNode->next;
+	}
+	return false;
+}
--- PCCOP/src/ama.h
+++ PCCOP/src/ama.h
@@ -7,24 +7,36 @@
 #include "common.h"
 #ifndef AMA_H_
 #define AMA_H_
+
+#define AMASIZE 1000 //ama.list大小
+#define AMAPOOLSIZE 1000 //ama poll大小
 typedef struct AMANode{
 	int state;
-	int *next;
+	AMANode *next;
 };
-
-extern int *finalStateArray;
-
 typedef struct AMAHead{
 	/* from 表示链表的头结点 表现为一个格局的编码 类型long int
 	 * tail 指向链表的尾部节点 表现为一个state的编码 类型是int的指针
 	 * */
 	long int from;
-	int *tail;
-	AMANode *next;
-}*AMA;
+	//AMANode *tail;//尾指针
+	AMANode head; //记录头     初始化时要将尾指针指向头结点的地址
+	AMANode *tail;
+	int mutex;//互斥量
+}AMAList;
+typedef struct AMA{
+	int count;
+	AMAList *list;
+};
+
 
 /*为链表预先开设的空间*/
 typedef struct Pool{
+	/**
+	 * size: pool的大小
+	 * head: pool的开始位置
+	 * tail: pool的最后一个非空元素的位置
+	 */
 	AMANode *item;
 	int size;
 	int head;
@@ -32,22 +44,24 @@ typedef struct Pool{
 };
 
 /*保存最近两次的自动机*/
-AMA ama_1;
-AMA ama_2;
+//AMA *ama_1;
+//AMA *ama_2;
 
 /*判断是否两个自动机是否相等*/
-bool isEqual(AMA ama_1,AMA ama_2);
-
+bool isEqual(AMA *ama_1,AMA *ama_2);
 /*插入一个trans到ama*/
-void insertTransToAMA(Trans t,AMA ama);
-
+void insertTransToAMA(Trans t,AMA *ama,Pool *pool);
+__device__ void d_insertTransToAMA(Trans t,AMA *ama,Pool *pool);
 /*删除ama*/
-void deleteAMA(AMA ama,Pool pool);
+void deleteAMA(AMA *ama,Pool *pool);
+
 
 /*判断ama中是否存在trans*/
-bool isTransInAMA(Trans t,AMA ama);
+bool isTransInAMA(Trans t,AMA *ama);
+
 
 /*更新AMA中state上标*/
-void updateAMA(AMA ama);
+void updateAMA(AMA *ama);
+
 
 #endif /* AMA_H_ */
